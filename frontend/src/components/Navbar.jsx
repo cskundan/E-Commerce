d2ad6b57fa35f975ec41b2203319cb4b3d5ffcdd
cskundan <cskundan32@gmail.com>
2026-08-19 00:24:35 +0530

    Update url

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   const logoutHandler = async () => {
     try {
       const res = await axios.post(
-        `http://localhost:8000/api/v1/user/logout`,
+        `https://e-commerce-4b4h.onrender.com/api/v1/user/logout`,
         {},
         {
           headers: {
